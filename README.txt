badb3816a5f0e77bb57ca29d4f23e304b334bf6a
Danielle Monroe <36176979+L0TU5@users.noreply.github.com>
2018-05-09 16:26:56 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/L0TU5/pen/eMoNVm.
+
+ A Star Trek take on the classic Simon Says game. WIP
---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+captainSeq = [];
+ensignSeq = [];
+var consoleSounds = [
+	"http://www.trekcore.com/audio/computer/keyok1.mp3", //A1
+	"http://www.trekcore.com/audio/computer/keyok2.mp3", //B2
+	"http://www.trekcore.com/audio/computer/keyok3.mp3", //C3
+	"http://www.trekcore.com/audio/computer/keyok4.mp3", //D4
+	"http://www.trekcore.com/audio/computer/input_failed2_clean.mp3", //Non-strict error
+	"http://www.trekcore.com/audio/computer/consolewarning.mp3" //strict error
+];
+
+//start game
+//generate captainSeq 
+//record ensignSeq
+//update level
